553bac1ced8f763a39036aea644de6e22d769fb6
jjangha <gkdms6128@gmail.com>
2023-08-11 10:30:07 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoInsert from './components/TodoInsert';
+import {useCallback, useRef, useState} from 'react';
+import TodoList from './components/TodoList';
+import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+
+ 
+    const [todos, setTodos] = useState([
+      {
+        id: 1,
+      text: '운동하기',
+      checked: true,
+      },
+      {
+        id: 2,
+      text: '요리하기',
+      checked: true,
+      },
+      {
+        id: 3,
+      text: '학원가기',
+      checked: false,
+      }
+    ]);
+
+    const nextId = useRef(4);
+    console.log(nextId);
+
+    const onInsert = useCallback( (value) => {
+    const todo = {
+      id: nextId.current,
+      text: value,
+      checked: false,
+    }
+    setTodos(todos.concat(todo));
+    nextId.current += 1;
+  },[todos])
+
+  const onToggle = useCallback( (id) => {
+    setTodos(todos.map(todo=>todo.id === id ? {...todo, checked: !todo.checked} : todo)) // ...스프라이트 연산자, todo객체의 속성들을 가져오는 것. 그래서 ...todo 속성값을 가져오라고 하는 것.
+  },[todos])
+
+  const onRemove = useCallback( (id) => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  },[todos])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onToggle={onToggle}onRemove={onRemove}/>
+    </TodoTemplate>
   );
-}
+  }
 
 export default App;
---
+++ src/components/TodoInsert.js
@@ -0,0 +1,27 @@
+import React, { useCallback, useState } from 'react'
+import { MdAdd } from "react-icons/md";
+import '../styles/TodoInsert.scss'
+
+function TodoInsert({onInsert}) {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback( (e) => {
+    console.log(e);
+    setValue(e.target.value); //input에 입력한 값
+  },[value])
+
+  const onSubmit = useCallback( (e) => {
+    onInsert(value);
+    setValue('');
+    e.preventDefault(); //submit이벤트는 브라우저에서 새로고침을 발생시킨다.
+  },[value])
+  return (
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input type="text" placeholder='할일을 입력하세요' onChange={onChange} value={value}/>
+      <button type="submit"><MdAdd/></button>
+    </form>
+    )
+
+}
+
+export default TodoInsert
